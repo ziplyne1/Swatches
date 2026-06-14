89cbdd2da9dc4e39700c67ae669ccd9185d608c5
Pax Willoughby <kilter_waspish0l@icloud.com>
2026-06-14 13:52:48 -0700

    fix ci_script (last commit closes #4)

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-cat <<file > Swatches/Swatches/Resources/DeveloperSettings.xcconfig
+cat <<file > /Volumes/workspace/repository/Swatches/Swatches/Resources/DeveloperSettings.xcconfig
 DEVELOPMENT_TEAM = Q4JH4MFFKH
 PRODUCT_BUNDLE_IDENTIFIER[config=Debug] = com.ziplyne.swatchestool.debug
 PRODUCT_BUNDLE_IDENTIFIER[config=Release] = com.ziplyne.swatchestool
